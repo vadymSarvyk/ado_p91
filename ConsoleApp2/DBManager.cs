573aa450519a440b3cccd236a88cb2922679f57f
vadymSarvyk <kharlamenkovadym@gmail.com>
2022-02-21 20:07:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/DBManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class DBManager
+    {
+        SqlConnection sql=null;
+        public void CreateConnection() { sql= new SqlConnection();
+            sql.ConnectionString = @"Data Source=WIN-57O3QHS7B6A\SQLEXPRESS; initial catalog=bookstore; integrated security=true;";
+        }
+        public void CechConnection()
+        {
+            SqlCommand comman = new SqlCommand();
+            comman.Connection = sql;
+            comman.CommandText = "insert into books values('vasya','vadym','1111')";
+            try
+            {
+                sql.Open();
+                comman.ExecuteNonQuery();
+                Console.WriteLine("Connection successful");
+            }
+            finally
+            {
+                if(sql!=null)
+                sql.Close();
+            }
+           
+            
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBManager dBManager = new DBManager();
+            dBManager.CreateConnection();
+            dBManager.CechConnection();
+        }
+    }
+}
